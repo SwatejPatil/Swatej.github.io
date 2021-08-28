7ba512998e03a2dc7b382c1dce2625da1867c78e
Swatej Patil <81546709+SwatejPatil@users.noreply.github.com>
2021-08-28 09:47:06 -0700

    Update main.js

--- assets2/js/main.js
+++ assets2/js/main.js
@@ -104,7 +104,7 @@ let opt = {
   filename:     'SwatejPatilResume.pdf',
   image:        { type: 'jpeg', quality: 0.98 },
   html2canvas:  { scale: 4 },
-  jsPDF:        { format: 'a4', orientation: 'portrait' }
+  jsPDF:        { format: 'a3', orientation: 'portrait' }
 };
 
 /* Function to call areaCv and Html2Pdf options */
